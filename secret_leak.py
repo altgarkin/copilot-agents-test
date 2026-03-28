6b92ff04bfe0d1f9f3d7c90b0e96045aee1bf7ae
altgarkin <altgarkin20@gmail.com>
2026-03-28 19:03:03 +0300

    Update secret_leak.py

--- secret_leak.py
+++ secret_leak.py
@@ -7,9 +7,12 @@ load_dotenv()
 AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")
 
 def connect():
-  if not AWS_SECRET_KEY:
-    print("Error: AWS_SECRET_KEY is not set!")
-    return
-  print(f"Connecting with: {AWS_SECRET_KEY}")
+    if not AWS_SECRET_KEY:
+        print("Error: AWS_SECRET_KEY is not set!")
+        return
+    
+    # ŞİFREYİ MASKELİYORUZ (Sadece ilk 4 karakteri gösterip gerisini yıldızlıyoruz)
+    masked_key = AWS_SECRET_KEY[:4] + "********" 
+    print(f"Connecting with: {masked_key}")
 
 connect()
